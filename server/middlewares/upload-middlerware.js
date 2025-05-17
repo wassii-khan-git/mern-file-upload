075f99e96751c9f28e84d561f9a2d73427777701
wassii-khan <wassiikhan933@gmail.com>
2025-05-17 21:38:41 +0500

    Add multer middleware for file uploads with custom storage configuration

---
+++ server/middlewares/upload-middlerware.js
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) { 
+        return cb(null, './uploads')
+    },
+    filename: function (req, file, cb) { 
+        return cb(null, Date.now() + '-' + file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
+export default upload;
